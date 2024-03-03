d93a6b23ace4382f1d7b99ca9d4cbe3e4096dc64
evangelineMavrommati <emavro19@gmail.com>
2024-03-02 21:54:49 -0600

    fixtures done

--- api_client.py
+++ api_client.py
@@ -2,6 +2,7 @@ import requests
 import utils
 from dotenv import load_dotenv
 import os
+from datetime import date
 
 
 load_dotenv()
@@ -18,6 +19,65 @@ class ApiClient(object):
             headers={"X-Auth-Token": API_KEY},
         )
         if response.status_code == 200:
-            names = utils.get_names(response.json())
+            names = utils.humanize_competition_names(response.json())
             return names
+        else:
+            return {
+                "response_code": response.status_code,
+                "message": "Could not return competitions. Please try again later.",
+            }
 
+    def get_current_season_info(self, league):
+        response = requests.get(
+            "https://api.football-data.org/v4/competitions/ % s" % league,
+            headers={"X-Auth-Token": API_KEY},
+        )
+
+        if response.status_code == 200:
+            return response.json()["currentSeason"]
+        else:
+            return {
+                "response_code": response.status_code,
+                "message": "Could not return % s matchday. Please try again later."
+                % league,
+            }
+
+    def get_fixtures(self, league, season_info, matchday=None):
+        payload = {}
+        matchday = matchday
+        end_date = season_info["endDate"]
+
+        if matchday != None:
+            payload = {"matchday": matchday}
+        else:
+            today = date.today().strftime("%Y-%m-%d")
+            payload = {"dateFrom": today, "dateTo": end_date}
+
+        response = requests.get(
+            "https://api.football-data.org/v4/competitions/ % s/matches" % league,
+            headers={"X-Auth-Token": API_KEY},
+            params=payload,
+        )
+
+        if response.status_code == 200:
+            r = utils.humanize_fixture_info(response.json())
+            return r
+
+        else:
+            return {
+                "response_code": response.status_code,
+                "message": "Could not return {league} fixtures. Please try again later.",
+            }
+
+    def get_standings(self, league):
+        response = requests.get(
+            "https://api.football-data.org/v4/competitions/{league}/standings",
+            headers={"X-Auth-Token": API_KEY},
+        )
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {
+                "response_code": response.status_code,
+                "message": "Could not return {league} standings. Please try again later.",
+            }
